9ecca9f158631e37add214db4335dafd0cc1f02b
XFX <sah0329@mail.ru>
2022-07-15 21:54:16 +0500

    commit

---
+++ Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+    Задача 41: 
+    Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+*/
+
+Console.Write("Сколько вы хотели написать количество чисел?: ");
+int n = Convert.ToInt32(Console.ReadLine()),
+    x = 0;
+int[] Num = new int[n];
+
+for (int i = 0; i <= n - 1; i++)
+{
+    Console.Write("введите число ", i + 1, ": ");
+    Num[i] = Convert.ToInt32(Console.ReadLine());
+    if (Num[i] > 0)
+        x++;
+}
+
+
+Console.WriteLine("[ " + string.Join(", ", Num) + " ]");
+
+Console.Write("Количество положительных чисел: ");
+Console.WriteLine(x);
---
+++ Task_2/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+    Задача 43: 
+    Напишите программу, которая найдёт точку пересечения двух прямых, 
+    заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+    значения b1, k1, b2 и k2 задаются пользователем.
+*/
+
+Console.Write("Введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * ((b2 - b1) / (k1 - k2)) + b1;
+
+Console.Write("[ ");
+Console.Write(x);
+Console.Write(" ; ");
+Console.Write(y);
+Console.Write(" ]");
+
+
+
+
+
+
+
+
+
+
+
+/*
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * ((b2 - b1) / (k1 - k2)) + b1;
+
+Console.Write(x);
+Console.Write(y);
+
+
+
+
+double y1 = k1*x+b1;
+double y2 = k2*x+b1;
+
+Console.Write(y1);
+Console.Write(y2);
+
+
+double x = (b2 - b1) / (k1 - k2);
+Console.Write(x);
+double y = b1*x+b2;
+Console.Write(y);
+
+
+*/
